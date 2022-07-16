cb609e00b07e3f63d8584b4e96efce0e2316b217
Erik Lunna <eslunna@gmail.com>
2022-07-16 21:53:29 +0200

    Allowed rabbits to be tamed with carrots (not rabid rabbits though.)

--- include/mondata.h
+++ include/mondata.h
@@ -686,15 +686,17 @@
    horses can be tamed by always-veggy food or lichen corpses but
    not tamed or pacified by other corpses or tins of veggy critters */
 #define befriend_with_obj(ptr, obj) \
-    (((ptr) == &mons[PM_MONKEY] || (ptr) == &mons[PM_APE])                   \
-     ? (obj)->otyp == BANANA                                                 \
-     : ((is_domestic(ptr) || (is_rat(ptr) && Role_if(PM_CONVICT))            \
-         || ((ptr) == &mons[PM_WARG] && Race_if(PM_ORC))                     \
-         || ((ptr) == &mons[PM_SABER_TOOTHED_TIGER] && Role_if(PM_CAVEMAN))) \
-        && (obj)->oclass == FOOD_CLASS                                       \
-        && ((ptr)->mlet != S_UNICORN                                         \
-            || obj->material == VEGGY                                        \
-            || ((obj)->otyp == CORPSE && (obj)->corpsenm == PM_LICHEN))))
+    (((ptr) == &mons[PM_MONKEY] || (ptr) == &mons[PM_APE]) \
+     ? (obj)->otyp == BANANA \
+     : ((is_domestic(ptr) \
+        || (is_rat(ptr) && Role_if(PM_CONVICT)) \
+        || ((ptr) == &mons[PM_WARG] && Race_if(PM_ORC)) \
+        || ((ptr) == &mons[PM_RABBIT] && (obj)->otyp == CARROT)\
+        || ((ptr) == &mons[PM_SABER_TOOTHED_TIGER] && Role_if(PM_CAVEMAN))) \
+     && (obj)->oclass == FOOD_CLASS \
+     && ((ptr)->mlet != S_UNICORN \
+        || obj->material == VEGGY \
+        || ((obj)->otyp == CORPSE && (obj)->corpsenm == PM_LICHEN))))
 
 /* Noise that a monster makes when engaged in combat. Assume that vocalizations
  * account for some noise, so monsters capable of vocalizing make more.
