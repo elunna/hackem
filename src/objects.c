588cf7a9f275ac84acff0e80f22990cb3690ddfe
Erik Lunna <eslunna@gmail.com>
2022-07-26 22:47:42 +0200

    Imported object listing for torch.

--- src/objects.c
+++ src/objects.c
@@ -791,11 +791,19 @@ TOOL("drum of earthquake","drum", 0, 0, 1, 1,   2,  25,  25, LEATHER, HI_LEATHER
            BITS(kn, 0, 1,chg, mgc, 1, 0, 0, bi, 0, hitbon, sub, mat),    \
            0, TOOL_CLASS, prob, 0, wt, cost, sdam, ldam, hitbon, 0, wt, clr)
 /* tools useful as weapons */
-WEPTOOL("pick-axe", None, 1, 0, 0, 0, 20, 100,  50,  6,  3, 0, WHACK,  P_PICK_AXE, IRON, HI_METAL),
-WEPTOOL("fishing pole", None, 1, 0, 0, 0,  5,  30,  50,  2,  6, 0, WHACK,  P_POLEARMS, METAL, HI_METAL),
+WEPTOOL("pick-axe", None,              1, 0, 0, 0, 20, 100,  50,  6,  3, 0, WHACK,  P_PICK_AXE, IRON, HI_METAL),
+WEPTOOL("fishing pole", None,          1, 0, 0, 0,  5,  30,  50,  2,  6, 0, WHACK,  P_POLEARMS, METAL, HI_METAL),
 WEPTOOL("grappling hook", "iron hook", 0, 0, 0, 0,  5,  30,  50,  2,  6, 0, WHACK,  P_FLAIL,    IRON, HI_METAL),
-WEPTOOL("unicorn horn", None, 1, 1, 0, 0,  0,  20, 100,  8, 10, 1, PIERCE, P_UNICORN_HORN, BONE, CLR_WHITE),
+WEPTOOL("unicorn horn", None,          1, 1, 0, 0,  0,  20, 100,  8, 10, 1, PIERCE, P_UNICORN_HORN, BONE, CLR_WHITE),
         /* 3.4.1: unicorn horn left classified as "magic" */
+WEPTOOL("torch", None,                 1, 0, 0, 0,  0,  25,   8,  5,  2, 0, WHACK,  P_CLUB, WOOD, HI_WOOD),
+#if 0
+OBJECT(OBJ("torch", (char *)0),
+	BITS(1,1,1,0,0,1,0,0,0,0,WHACK,P_CLUB,WOOD),
+	0, TOOL_CLASS, 25, 0,
+	20, 8, 2, 5, WHACK, 0, 20, HI_WOOD ),
+#endif
+
 /* two unique tools;
  * not artifacts, despite the comment which used to be here
  */
