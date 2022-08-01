40d3561e0972b288b1bcc86196f5b2e75c3d15f9
Erik Lunna <eslunna@gmail.com>
2022-08-01 12:46:53 +0200

    Changed pick-axe weight to 75 (matches slashem)

--- src/objects.c
+++ src/objects.c
@@ -803,7 +803,7 @@ TOOL("drum of earthquake","drum", 0, 0, 1, 1,   2,  25,  25, LEATHER, HI_LEATHER
            BITS(kn, 0, 1,chg, mgc, 1, 0, 0, bi, 0, hitbon, sub, mat),    \
            0, TOOL_CLASS, prob, 0, wt, cost, sdam, ldam, hitbon, 0, wt, clr)
 /* tools useful as weapons */
-WEPTOOL("pick-axe", None,              1, 0, 0, 0, 20, 100,  50,  6,  3, 0, WHACK,  P_PICK_AXE, IRON, HI_METAL),
+WEPTOOL("pick-axe", None,              1, 0, 0, 0, 20,  75,  50,  6,  3, 0, WHACK,  P_PICK_AXE, IRON, HI_METAL),
 WEPTOOL("fishing pole", None,          1, 0, 0, 0,  5,  30,  50,  2,  6, 0, WHACK,  P_POLEARMS, METAL, HI_METAL),
 WEPTOOL("grappling hook", "iron hook", 0, 0, 0, 0,  5,  30,  50,  2,  6, 0, WHACK,  P_FLAIL,    IRON, HI_METAL),
 WEPTOOL("unicorn horn", None,          1, 1, 0, 0,  0,  20, 100,  8, 10, 1, PIERCE, P_UNICORN_HORN, BONE, CLR_WHITE),
