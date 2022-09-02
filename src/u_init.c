e645e56c40d77b746432d85ac7aed53f14c881bf
Erik Lunna <eslunna@gmail.com>
2022-09-02 10:33:17 +0200

    Flame mages have Dagger max skill reduced to Basic.

--- src/u_init.c
+++ src/u_init.c
@@ -466,7 +466,7 @@ static const struct def_skill Skill_Con[] = {
 
 static const struct def_skill Skill_F[] = {
 /*Style: small-med edged weapons, blunt weapons*/
-    { P_DAGGER, P_EXPERT },             { P_KNIFE,  P_SKILLED },
+    { P_DAGGER, P_BASIC },             { P_KNIFE,  P_SKILLED },
     { P_AXE, P_BASIC },                 /*{ P_PICK_AXE, P_BASIC },*/
     { P_SHORT_SWORD, P_EXPERT },        /*{ P_BROAD_SWORD, P_BASIC },*/
     /*{ P_LONG_SWORD, P_SKILLED },*/        /*{ P_TWO_HANDED_SWORD, P_BASIC },*/
