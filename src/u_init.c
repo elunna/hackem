e47923e5b3367ae9729d8ac7083c0dfe07b9c823
Erik Lunna <eslunna@gmail.com>
2022-09-02 10:36:11 +0200

    Flame mages have Short Sword max skill reduced to Skilled.

--- src/u_init.c
+++ src/u_init.c
@@ -468,7 +468,7 @@ static const struct def_skill Skill_F[] = {
 /*Style: small-med edged weapons, blunt weapons*/
     { P_DAGGER, P_BASIC },             { P_KNIFE,  P_SKILLED },
     { P_AXE, P_BASIC },                 /*{ P_PICK_AXE, P_BASIC },*/
-    { P_SHORT_SWORD, P_EXPERT },        /*{ P_BROAD_SWORD, P_BASIC },*/
+    { P_SHORT_SWORD, P_SKILLED },        /*{ P_BROAD_SWORD, P_BASIC },*/
     /*{ P_LONG_SWORD, P_SKILLED },*/        /*{ P_TWO_HANDED_SWORD, P_BASIC },*/
     { P_SCIMITAR, P_SKILLED },          /*{ P_SABER, P_SKILLED },*/
     /* { P_PADDLE, P_BASIC }, */
