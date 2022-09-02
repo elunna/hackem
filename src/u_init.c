452188812fa53117f8c4aa79a08263fba8b18ad9
Erik Lunna <eslunna@gmail.com>
2022-09-02 11:18:12 +0200

    Flame Mage: Removed dagger and knife skills

--- src/u_init.c
+++ src/u_init.c
@@ -466,17 +466,16 @@ static const struct def_skill Skill_Con[] = {
 
 static const struct def_skill Skill_F[] = {
 /*Style: small-med edged weapons, blunt weapons*/
-    { P_DAGGER, P_BASIC },              { P_KNIFE,  P_SKILLED },
+    /*{ P_DAGGER, P_BASIC },*/        /*{ P_KNIFE,  P_SKILLED },*/
     { P_AXE, P_BASIC },               /*{ P_PICK_AXE, P_BASIC },*/
     { P_SHORT_SWORD, P_SKILLED },     /*{ P_BROAD_SWORD, P_BASIC },*/
     /*{ P_LONG_SWORD, P_SKILLED },*/  /*{ P_TWO_HANDED_SWORD, P_BASIC },*/
     /*{ P_SCIMITAR, P_SKILLED },*/    /*{ P_SABER, P_SKILLED },*/
-    /* { P_PADDLE, P_BASIC }, */
     { P_MACE, P_BASIC },              /*{ P_MORNING_STAR, P_BASIC },
     { P_FLAIL, P_BASIC },*/             { P_HAMMER, P_BASIC },
     { P_QUARTERSTAFF, P_EXPERT },    /* { P_POLEARMS, P_BASIC },*/
 /* Relies on spells for ranged attack
-    { P_SPEAR, P_BASIC },               { P_JAVELIN, P_BASIC },
+    { P_SPEAR, P_BASIC },               
     { P_TRIDENT, P_BASIC },             { P_LANCE, P_BASIC },
     { P_BOW, P_BASIC },                 { P_SLING, P_BASIC },
     { P_CROSSBOW, P_BASIC },            { P_DART, P_EXPERT },
