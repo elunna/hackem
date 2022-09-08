14f898ce7cf56b2b0a4a730f18eb38dec29f9ffd
Erik Lunna <eslunna@gmail.com>
2022-09-08 10:13:06 +0200

    Revert "Add 3 alignment keys..."
    This reverts commit 0e0ddd0ed1a36a84a435e846dae871d8031c7bc5.

--- include/artilist.h
+++ include/artilist.h
@@ -345,21 +345,8 @@ STATIC_OVL NEARDATA struct artifact artilist[] = {
     A("Werebane", SABER, (SPFX_RESTR | SPFX_WARN | SPFX_DFLAGH), 0, MH_WERE,
       PHYS(5, 0), DFNS(AD_WERE), NO_CARY, 0, A_NONE, NON_PM, NON_PM, 1500L,
       NO_COLOR),
-    
-    /*** Alignment artifact keys ***/
-    
-    A("The Key of Law", SKELETON_KEY,
-      (SPFX_NOGEN | SPFX_RESTR | SPFX_INTEL), 0, 0,
-      NO_ATTK, NO_DFNS, NO_CARY, 0, A_LAWFUL, NON_PM, NON_PM, 1000L, NO_COLOR ),
 
-    A("The Key of Neutrality", SKELETON_KEY,
-      (SPFX_NOGEN | SPFX_RESTR | SPFX_INTEL), 0, 0,
-      NO_ATTK, NO_DFNS, NO_CARY, 0, A_NEUTRAL, NON_PM, NON_PM, 1000L, NO_COLOR ),
 
-    A("The Key of Chaos", SKELETON_KEY,
-      (SPFX_NOGEN | SPFX_RESTR | SPFX_INTEL), 0, 0,
-      NO_ATTK, NO_DFNS, NO_CARY, 0, A_CHAOTIC, NON_PM, NON_PM, 1000L, NO_COLOR ),
-    
     /*
      *      The artifacts for the quest dungeon, all self-willed.
      */
