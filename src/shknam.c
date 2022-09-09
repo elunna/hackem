da35c1b63a30f9a3bf73d5e791c04f7602daa55c
Erik Lunna <eslunna@gmail.com>
2022-09-09 14:09:30 +0200

    Reformatted the shopkeeper services code; corrected indentation, etc.

--- src/shknam.c
+++ src/shknam.c
@@ -1086,45 +1086,45 @@ static void
 init_shk_services(shk)
 struct monst *shk;
 {
-	ESHK(shk)->services = 0L;
+    ESHK(shk)->services = 0L;
 
-	/* KMH, balance patch 2 -- Increase probability of shopkeeper services.
-	 * Requested by Dave <mitch45678@aol.com>
-	 */
-	if (Is_blackmarket(&u.uz)) {
-		ESHK(shk)->services = 
-		    SHK_ID_BASIC|SHK_ID_PREMIUM|SHK_UNCURSE|SHK_APPRAISE|
-		    SHK_SPECIAL_A|SHK_SPECIAL_B|SHK_SPECIAL_C;
-		return;
-	}
+    /* KMH, balance patch 2 -- Increase probability of shopkeeper services.
+     * Requested by Dave <mitch45678@aol.com>
+     */
+    if (Is_blackmarket(&u.uz)) {
+            ESHK(shk)->services = 
+                SHK_ID_BASIC|SHK_ID_PREMIUM|SHK_UNCURSE|SHK_APPRAISE|
+                SHK_SPECIAL_A|SHK_SPECIAL_B|SHK_SPECIAL_C;
+            return;
+    }
 
-	/* Guarantee some form of identification
-	 * 1/3 		both Basic and Premium ID
-	 * 2/15 	Premium ID only
-	 * 8/15 	Basic ID only
-	 */
-	if (!rn2(2)) ESHK(shk)->services |= (SHK_ID_BASIC|SHK_ID_PREMIUM);
-	else if (!rn2(4)) ESHK(shk)->services |= SHK_ID_PREMIUM;
-	else ESHK(shk)->services |= SHK_ID_BASIC;
+    /* Guarantee some form of identification
+     * 1/3 		both Basic and Premium ID
+     * 2/15 	Premium ID only
+     * 8/15 	Basic ID only
+     */
+    if (!rn2(2)) ESHK(shk)->services |= (SHK_ID_BASIC|SHK_ID_PREMIUM);
+    else if (!rn2(4)) ESHK(shk)->services |= SHK_ID_PREMIUM;
+    else ESHK(shk)->services |= SHK_ID_BASIC;
 
-	if (!rn2(3)) ESHK(shk)->services |= SHK_UNCURSE;
+    if (!rn2(3)) ESHK(shk)->services |= SHK_UNCURSE;
 
-	if (!rn2(3) && shk_class_match(WEAPON_CLASS, shk))
-		ESHK(shk)->services |= SHK_APPRAISE;
+    if (!rn2(3) && shk_class_match(WEAPON_CLASS, shk))
+            ESHK(shk)->services |= SHK_APPRAISE;
 
-	if ((shk_class_match(WEAPON_CLASS, shk) == SHK_MATCH) ||
-	(shk_class_match(ARMOR_CLASS, shk) == SHK_MATCH) ||
-	(shk_class_match(WAND_CLASS, shk) == SHK_MATCH) ||
-	(shk_class_match(TOOL_CLASS, shk) == SHK_MATCH) ||
-	(shk_class_match(SPBOOK_CLASS, shk) == SHK_MATCH) ||
-	(shk_class_match(RING_CLASS, shk) == SHK_MATCH)) {
-		if (!rn2(4/*5*/)) ESHK(shk)->services |= SHK_SPECIAL_A;
-		if (!rn2(4/*5*/)) ESHK(shk)->services |= SHK_SPECIAL_B;
-	}
-	if (!rn2(4/*5*/) && (shk_class_match(WEAPON_CLASS, shk) == SHK_MATCH))
-	 ESHK(shk)->services |= SHK_SPECIAL_C;
+    if ((shk_class_match(WEAPON_CLASS, shk) == SHK_MATCH) ||
+    (shk_class_match(ARMOR_CLASS, shk) == SHK_MATCH) ||
+    (shk_class_match(WAND_CLASS, shk) == SHK_MATCH) ||
+    (shk_class_match(TOOL_CLASS, shk) == SHK_MATCH) ||
+    (shk_class_match(SPBOOK_CLASS, shk) == SHK_MATCH) ||
+    (shk_class_match(RING_CLASS, shk) == SHK_MATCH)) {
+            if (!rn2(4/*5*/)) ESHK(shk)->services |= SHK_SPECIAL_A;
+            if (!rn2(4/*5*/)) ESHK(shk)->services |= SHK_SPECIAL_B;
+    }
+    if (!rn2(4/*5*/) && (shk_class_match(WEAPON_CLASS, shk) == SHK_MATCH))
+     ESHK(shk)->services |= SHK_SPECIAL_C;
 
-	return;
+    return;
 }
 
 
