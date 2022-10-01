0f80de0b7534d9fe8855609a1258c34921eabddb
Erik Lunna <eslunna@gmail.com>
2022-10-01 23:53:04 +0200

    Added tile for goggles and reordered monster and object tiles.

--- order.sh
+++ order.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Re-order monsters.txt
+awk -f win/share/tile-renumberer.awk win/share/monsters.txt > win/share/mtmp.txt
+rm -v win/share/monsters.txt
+mv -v win/share/mtmp.txt win/share/monsters.txt
+
+# Re-order objects.txt
 awk -f win/share/tile-renumberer.awk win/share/objects.txt > win/share/otmp.txt
 rm -v win/share/objects.txt
 mv -v win/share/otmp.txt win/share/objects.txt
