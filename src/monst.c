1579b276370f7403bbe5fdca178a30321d7e7980
Erik Lunna <eslunna@gmail.com>
2022-10-05 14:46:46 +0200

    Imported will-o'-the-wisp (from splice) - there is also a version in slashthem, but the splice version is a bit more true to D&D style so I went with that.

--- src/monst.c
+++ src/monst.c
@@ -2811,6 +2811,19 @@ NEARDATA struct permonst mons[] = {
     /*
      * lights
      */
+    MON("will-o'-the-wisp", S_LIGHT, 
+        LVL(2, 9, 2, 0, 0), (G_NOCORPSE | G_SGROUP | G_GENO | 4),
+        A(ATTK(AT_TUCH, AD_BLND, 1, 2), 
+          ATTK(AT_TUCH, AD_ELEC, 1, 1),
+          NO_ATTK, NO_ATTK, NO_ATTK, NO_ATTK),
+        SIZ(0, 0, MS_SILENT, MZ_SMALL),
+        MR_FIRE | MR_COLD | MR_ELEC | MR_DISINT | MR_SLEEP | MR_POISON
+            | MR_ACID | MR_STONE, 0, 
+        M1_FLY | M1_BREATHLESS | M1_AMORPHOUS | M1_NOEYES | M1_NOLIMBS
+               | M1_NOHEAD | M1_MINDLESS | M1_UNSOLID | M1_NOTAKE,
+        M2_HOSTILE | M2_NEUTER | M2_WANDER, 
+        M3_INFRAVISIBLE, 0, 0, 4, CLR_WHITE),
+    
     MON("yellow light", S_LIGHT,
         LVL(3, 15, 0, 0, 0), (G_NOCORPSE | G_GENO | 4),
         A(ATTK(AT_EXPL, AD_BLND, 10, 20), 
