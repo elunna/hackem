7a74cbf0f6ed6a2ad43886811a2b5fe626f78e3d
Erik Lunna <eslunna@gmail.com>
2022-11-18 13:14:47 +0100

    Removed line that debugged enexto() called with mdat==0

--- src/teleport.c
+++ src/teleport.c
@@ -243,8 +243,6 @@ int start_range; /**< Distance of checked tiles to begin with. Should be >=1. */
 
     entflags &= ~GP_ALLOW_XY;
     if (!mdat) {
-        debug_pline("enexto() called with mdat==0");
-
         /* default to player's original monster type */
         mdat = &mons[u.umonster];
     }
