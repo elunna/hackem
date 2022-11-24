b67f859b32fe534a0e314ee288f39caafd89430a
Erik Lunna <eslunna@gmail.com>
2022-11-24 13:07:17 +0100

    Fix for heap-use-after-free when monster expels us onto a lev teleport trap. Fixes #94.

--- src/mhitu.c
+++ src/mhitu.c
@@ -475,7 +475,7 @@ boolean message;
     newsym(u.ux, u.uy);
     spoteffects(TRUE);
     /* to cover for a case where mtmp is not in a next square */
-    if (um_dist(mtmp->mx, mtmp->my, 1))
+    if (mtmp && um_dist(mtmp->mx, mtmp->my, 1))
         pline("Brrooaa...  You land hard at some distance.");
 }
 
