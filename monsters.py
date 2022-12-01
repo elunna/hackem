aed6f77a7ccc768a9ee624e6df10c26fcf24d690
Erik Lunna <eslunna@gmail.com>
2022-12-01 06:30:49 +0100

    Revision of monster scraper - all monster info is now extracted.

--- monsters.py
+++ monsters.py
@@ -7,51 +7,102 @@ import math
 # Need parse_siz
 # Need parse_flags
 
+# Extract from parentheses
+
 def stripcomments(text):
     return re.sub('//.*?\n|/\*.*?\*/', '', text, flags=re.S)
 
+def extract_parens(text):
+    return text[text.find("(") + 1:text.find(")")]
+
+def strip_list(l):
+    return [i.strip() for i in l]
+
+def split_by_commas(text):
+    #splitup = re.split(r',(?![^()]*\))', text)
+    return re.split(r',\s*(?![^()]*\))', text)
+
+
+# Remove any parentheses(with content)
+def purge_parens(text):
+    return re.sub(r'\(.*?\)', '', text)
+
+def extract_allparens(text):
+    """ Looks through a string and extracts all text contained in parentheses,
+        and returns a list with a string for each parentheses found. Discards
+        the parens.
+    """
+    #  Stackoverflow: re.findall('\(.*?\)',s)
+    #  parens = re.findall(r'\(.*?\)+', text)    # This keeps the outer parens
+
+    # Note: Regex doesn't like newlines, text should be cleaned of them before this.
+    parens = re.findall(r'\((.*?)\)+', text)  # Discard the parentheses
+    return parens
+
 def extract_data(text):
     mondat = {}
-    #splitup = re.split(r',(?![^()]*\))', text)
-    splitup = re.split(r',\s*(?![^()]*\))', text)
-    # Parse these things
-    # Split commas not in parentheses?
+    # Initial cleanup
+    text = text.strip()
+    text = text.lstrip("MON(")
+    text = text.rstrip(',')
+    text = text.rstrip(')')
+    text = text.replace("A(ATTK", "ATTK")
+    text = text.replace("NO_ATTK)", "NO_ATTK")
+    
+    parens = extract_allparens((text))
+    text = purge_parens(text)
+
+    splitup = split_by_commas(text)
+    
     # Name
-    mondat['name'] = splitup[0]
+    mondat['name'] = splitup[0].strip("\"")
+    
     # Symbol
     mondat['sym'] = splitup[1]
+    
     # LVL - Always has ()
-    mondat['sym'] = splitup[2]
-    # GEN FLAGS - sometimes has ()
-    mondat['gen'] = splitup[3]
-    # Attacks - always has nested ()
-    mondat['attacks'] = splitup[4]
+    # 5 fields: base level, speed, AC, MR, alignment)
+    # lvl = extract_parens(splitup[2]).split(',')
+    lvl = strip_list(parens.pop(0).split(','))
+    mondat['base level'] = lvl[0]
+    mondat['speed'] = lvl[1]
+    mondat['AC'] = lvl[2]
+    mondat['MR'] = lvl[3]
+    mondat['alignment'] = lvl[4]
 
-    s = splitup[5]
-    commasplit = s.split(',')
-    # print(re.search(r'\((.*?)\)', s).group(1))
-    
+    # GEN FLAGS - sometimes has ()
+    # gen = extract_parens(splitup[3]).split(',')[0]
+    gen = parens.pop(0)
+    mondat['gen'] = strip_list(gen.split('|'))
+
+
     # Size - Always has ()
-    mondat['siz'] = s[s.find("(")+1:s.find(")")]
+    # Fields: weight, nut, sound, size
+    siz = strip_list(parens.pop(-1).split(','))
+    mondat['weight'] = siz[0]
+    mondat['nutrition'] = siz[1]
+    mondat['sound'] = siz[2]
+    mondat['size'] = siz[3]
+
+    # Attacks - always has nested ()
+    # Anything left over is attacks.
+    mondat['attacks'] = parens 
+
+    # Misc Flags
+    # M1, M2, M3, M4, flg3, flg4, mflg, 
+    flags = []
+    for i in range(-9, -2):
+        for f in splitup[i].split('|'):
+            f = f.strip()
+            if f != '0':
+                flags.append(f)
+    mondat['flags'] = flags
     
-    # M1
-    mondat['mr1'] = commasplit[-9]
-    # M2
-    mondat['mr2'] = commasplit[-8]
-    # M3
-    mondat['flg1'] = commasplit[-7]
-    # M4
-    mondat['flg2'] = commasplit[-6]
-    # flg3, 
-    mondat['flg3'] = commasplit[-5]
-    # flg4,
-    mondat['flg4'] = commasplit[-4]
-    # mflg, 
-    mondat['mflg'] = commasplit[-3]
     # difficulty
-    mondat['difficulty'] = commasplit[-2]
+    mondat['difficulty'] = splitup[-2].strip()
+    
     # Color 
-    mondat['color'] = commasplit[-1]
+    mondat['color'] = splitup[-1].strip()
 
     return mondat
         
@@ -83,8 +134,10 @@ def main():
     monsters.pop(0)
     monsters.pop(0)
     
-    for key, val in extract_data(monsters[0]).items():
-        print(key, val)
+    for m in range(50):
+        for key, val in extract_data(monsters[m]).items():
+            print("{:15} {}".format(key, val))
+        print("--------------------------")
 
 
 
