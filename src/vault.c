797a0f15d0aabacf6d785eb69fb2690f50c21bf5
Erik Lunna <eslunna@gmail.com>
2022-12-02 00:36:06 +0100

    Convicts can wear blindfolds or towels to disguise themselves from watchguards.

--- src/vault.c
+++ src/vault.c
@@ -424,7 +424,10 @@ invault()
             mongone(guard);
             return;
         }
-        if (Role_if(PM_CONVICT) && !Upolyd) {
+        if (Role_if(PM_CONVICT) && (!Upolyd 
+                                    || (ublindf 
+                                        && (ublindf->otyp != TOWEL 
+                                            && ublindf->otyp != BLINDFOLD)))) {
             setmangry(guard, FALSE);
             verbalize("I saw your picture on the wanted poster!");
 	    if (!MON_WEP(guard)) {
