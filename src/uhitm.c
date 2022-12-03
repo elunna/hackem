cd608d634f787c68b2790896da2d268d8a6da9fa
Erik Lunna <eslunna@gmail.com>
2022-12-03 17:23:08 +0100

    Fix: sling stone instakill triggered on non-critical hits(xnh)

--- src/uhitm.c
+++ src/uhitm.c
@@ -1150,7 +1150,7 @@ int dieroll;
                 else if (mdat->mlet == S_GIANT && uslinging()
                          && thrown == HMON_THROWN
                          && ammo_and_launcher(obj, uwep)
-                         && P_SKILL(P_SLING) >= P_SKILLED && dieroll > 1
+                         && P_SKILL(P_SLING) >= P_SKILLED && dieroll == 1
                          && !rn2(P_SKILL(P_SLING) == P_SKILLED ? 2 : 1)) {
                     /* With a critical hit, a skilled slinger can bring down
                      * even the mightiest of giants. */
