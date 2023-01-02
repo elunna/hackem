46ab77298d4c4dc2f2e238e0500952b757982052
Erik Lunna <eslunna@gmail.com>
2023-01-02 06:57:53 +0100

    Greatly increase odds of sewage vs water rivers forming in the Gnomish Mines. (evil)

--- src/mkmap.c
+++ src/mkmap.c
@@ -593,7 +593,7 @@ STATIC_OVL void
 mkrivers()
 {
     int nriv = rn2(3) + 1;
-    boolean notpool = rn2(100) < depth(&u.uz);
+    boolean notpool = rn2(20) < depth(&u.uz);
 
     while (nriv--) {
         if (rn2(2))
