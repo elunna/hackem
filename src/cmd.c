ebb6eed72c6b9eee1fb92adb4b269a328935277e
Erik Lunna <eslunna@gmail.com>
2023-01-08 14:32:58 +0100

    Fix: stairtravel was not working when known stairs were covered up by objects. Fixes #145.

--- src/cmd.c
+++ src/cmd.c
@@ -6776,16 +6776,22 @@ find_remembered_stairs(boolean upstairs, coord *cc)
 
     /* We can't reference the stairs directly because mimics can mimic fake
        ones. */
+    int k;
     for (x = 0; x < COLNO; x++) {
         for (y = 0; y < ROWNO; y++) {
-            if (glyph_to_cmap(level.locations[x][y].glyph) == stair ||
-                glyph_to_cmap(level.locations[x][y].glyph) == sladder ||
-                glyph_to_cmap(level.locations[x][y].glyph) == sbranch) {
-                if (found_stairs == 0) {
-                    cc->x = x;
-                    cc->y = y;
+            if (levl[x][y].seenv) {
+                k = back_to_glyph(x, y);
+                
+                if (glyph_is_cmap(k) && 
+                    (glyph_to_cmap(k) == stair 
+                     || glyph_to_cmap(k) == sladder 
+                     || glyph_to_cmap(k) == sbranch)) {
+                    if (found_stairs == 0) {
+                        cc->x = x;
+                        cc->y = y;
+                    }
+                    found_stairs++;
                 }
-                found_stairs++;
             }
         }
     }
