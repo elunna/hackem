dce86f3f2aca5dfff074cea3cd648da1c9f088ca
Erik Lunna <eslunna@gmail.com>
2023-01-14 05:36:42 +0100

    Quest leaders will make you forfeit the quest artifact only if you heavily abuse alignment (less than -64 alignment abuse score required)

--- src/quest.c
+++ src/quest.c
@@ -265,7 +265,7 @@ struct obj *obj; /* quest artifact; possibly null if carrying Amulet */
         /* leader IDs the real amulet but ignores any fakes */
         if ((otmp = carrying(AMULET_OF_YENDOR)) != 0)
             fully_identify_obj(otmp);
-    } else if (u.ualign.abuse != 0) { /* player has abused their alignment */
+    } else if (u.ualign.abuse < -64) { /* player has heavily abused their alignment */
         /* the more often the player abuses their alignment,
            the greater the odds of their quest leader demanding
            that they forfeit the quest artifact */
