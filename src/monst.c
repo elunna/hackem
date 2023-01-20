2ea7a2f9037346729dbec19b3aa36bf8b34a5a74
Erik Lunna <eslunna@gmail.com>
2023-01-20 08:24:17 +0100

    Fix typo in last commit.

--- src/monst.c
+++ src/monst.c
@@ -824,7 +824,7 @@ NEARDATA struct permonst mons[] = {
         A(ATTK(AT_EXPL, AD_WIND, 3, 2), 
           NO_ATTK, NO_ATTK, NO_ATTK, NO_ATTK, NO_ATTK),
         SIZ(10, 10, MS_SILENT, MZ_SMALL), 
-        R_FIRE | MR_COLD | MR_ELEC | MR_DISINT | MR_SLEEP | MR_POISON
+        MR_FIRE | MR_COLD | MR_ELEC | MR_DISINT | MR_SLEEP | MR_POISON
             | MR_ACID | MR_STONE, 0,
         M1_FLY | M1_BREATHLESS | M1_NOLIMBS | M1_NOHEAD | M1_MINDLESS
             | M1_NOTAKE,
