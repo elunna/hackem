36ce00c4ba979e5a9049d8b18ff3767f37d9c085
Erik Lunna <eslunna@gmail.com>
2023-04-10 08:23:36 +0200

    Fix: inconsistent attack behavior while submerged in water (EvilHack).

--- src/mhitu.c
+++ src/mhitu.c
@@ -616,7 +616,9 @@ register struct monst *mtmp;
 
     if (!ranged)
         nomul(0);
-    if (DEADMONSTER(mtmp) || (Underwater && !is_swimmer(mtmp->data)))
+    if (DEADMONSTER(mtmp)
+        || (Underwater && !(is_swimmer(mtmp->data)
+                            && is_pool(mtmp->mx, mtmp->my))))
         return 0;
 
     /* If swallowed, can only be affected by u.ustuck */
