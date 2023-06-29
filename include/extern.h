elunna/hackem

commit 0c505a05962b290ab4261ccb0dd11c360a034526

Fix: Potions display as bottles in inventory when playing as pirate (reported by mobileuser). Fixes #306

include/extern.h
@@ -2144,6 +2144,8 @@ E void FDECL(collect_spirit, (struct monst *, struct obj *));
 E void FDECL(dumplogmsg, (const char *));
 E void NDECL(dumplogfreemessages);
 #endif
+E char *FDECL(replace, (const char *, const char *, const char *));
+E char *FDECL(piratesay, (const char *));
 E void VDECL(pline, (const char *, ...)) PRINTF_F(1, 2);
 E void VDECL(custompline, (unsigned, const char *, ...)) PRINTF_F(2, 3);
 E void VDECL(Norep, (const char *, ...)) PRINTF_F(1, 2);

src/objnam.c
@@ -210,7 +210,7 @@ register int otyp;
         Strcpy(buf, "coin");
         break;
     case POTION_CLASS:
-        Strcpy(buf, "potion");
+        Strcpy(buf, Role_if(PM_PIRATE) ? "bottle": "potion");
         break;
     case SCROLL_CLASS:
         Strcpy(buf, "scroll");
@@ -273,6 +273,10 @@ register int otyp;
         xcalled(buf, BUFSZ - (dn ? (int) strlen(dn) + 3 : 0), "", un);
     if (dn)
         Sprintf(eos(buf), " (%s)", dn);
+        
+    if (Role_if(PM_PIRATE))
+        buf = replace(buf, "potion", "bottle");
+        
     return buf;
 }
 
@@ -955,13 +959,13 @@ unsigned cxn_flags; /* bitmask of CXN_xxx values */
         break;
     case POTION_CLASS:
         if (!dknown) {
-            Strcat(buf, "potion");
+            Strcat(buf, Role_if(PM_PIRATE) ? "bottle": "potion");
             break;
         }
         if (obj->odiluted)
             Strcat(buf, "diluted ");
         if (nn || un) {
-            Strcat(buf, "potion");
+            Strcat(buf, Role_if(PM_PIRATE) ? "bottle": "potion");
             if (nn) {
                 Strcat(buf, " of ");
                 if (typ == POT_WATER && bknown
@@ -974,7 +978,7 @@ unsigned cxn_flags; /* bitmask of CXN_xxx values */
             }
         } else {
             Strcat(buf, dn);
-            Strcat(buf, " potion");
+            Strcat(buf, Role_if(PM_PIRATE) ? " bottle": " potion");
         }
         break;
     case SCROLL_CLASS:

commit 17c2817595b401671fcf8c71d4ad3fe8c0ea5197

Fix: Broken replace definition from 0c505a059.

include/extern.h
@@ -2144,8 +2144,7 @@ E void FDECL(collect_spirit, (struct monst *, struct obj *));
 E void FDECL(dumplogmsg, (const char *));
 E void NDECL(dumplogfreemessages);
 #endif
-E char *FDECL(replace, (const char *, const char *, const char *));
-E char *FDECL(piratesay, (const char *));
+E const char *FDECL(replace, (const char *, const char *, const char *));
 E void VDECL(pline, (const char *, ...)) PRINTF_F(1, 2);
 E void VDECL(custompline, (unsigned, const char *, ...)) PRINTF_F(2, 3);
 E void VDECL(Norep, (const char *, ...)) PRINTF_F(1, 2);