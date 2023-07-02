60263a7a310faca48bdb7781ed8c057af8c0b982
Erik Lunna <eslunna@gmail.com>
2023-07-02 22:20:50 +0200

    Restore Tourists skill in twoweaponing to 'skilled'. (EvilHack)

--- src/u_init.c
+++ src/u_init.c
@@ -872,7 +872,7 @@ static const struct def_skill Skill_T[] = {
     { P_ENCHANTMENT_SPELL, P_BASIC },
     { P_ESCAPE_SPELL, P_SKILLED },
     { P_RIDING, P_BASIC },
-    { P_TWO_WEAPON_COMBAT, P_BASIC },
+    { P_TWO_WEAPON_COMBAT, P_SKILLED },
     { P_BARE_HANDED_COMBAT, P_SKILLED },
     { P_SHIELD, P_BASIC },
     { P_FIREARM, P_BASIC },
