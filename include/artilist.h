ce6e087402b8068742bfef427aa04c3c98537523
Erik Lunna <eslunna@gmail.com>
2023-07-12 00:10:13 +0200

    Revert luck blade back to a short sword (SLASH'EM).

--- include/artilist.h
+++ include/artilist.h
@@ -328,8 +328,8 @@ STATIC_OVL NEARDATA struct artifact artilist[] = {
       NON_PM, 10000L, NO_COLOR, DEFAULT_MAT),
 
     /* Convict first artifact weapon. Acts like a luckstone. */
-    A("Luck Blade", BROADSWORD, (SPFX_RESTR | SPFX_LUCK), 0, 0,
-      PHYS(5, 6), NO_DFNS, NO_CARY, 0, A_CHAOTIC, PM_CONVICT, NON_PM, 3000L,
+    A("Luck Blade", SHORT_SWORD, (SPFX_RESTR | SPFX_LUCK), 0, 0,
+      PHYS(5, 5), NO_DFNS, NO_CARY, 0, A_CHAOTIC, PM_CONVICT, NON_PM, 3000L,
       NO_COLOR, METAL),
 
     /*
