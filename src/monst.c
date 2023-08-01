fb0e71f147d94ec41d79e5ffff1f3cbb08880a1c
Erik Lunna <eslunna@gmail.com>
2023-08-02 00:34:14 +0200

    Move Death's weapon attack to be first in the attack chain.

--- src/monst.c
+++ src/monst.c
@@ -6853,9 +6853,9 @@ struct permonst _mons2[] = {
      */
     MON("Death", S_DEMON,
         LVL(40, 12, -5, 100, 0), (G_UNIQ | G_NOGEN),
-        A(ATTK(AT_TUCH, AD_DETH, 8, 8), 
+        A(ATTK(AT_WEAP, AD_PHYS, 1, 6),
           ATTK(AT_TUCH, AD_DETH, 8, 8), 
-          ATTK(AT_WEAP, AD_PHYS, 1, 6),
+          ATTK(AT_TUCH, AD_DETH, 8, 8),
           NO_ATTK, NO_ATTK, NO_ATTK),
         SIZ(WT_HUMAN, 1, MS_RIDER, MZ_HUMAN),
         MR_FIRE | MR_COLD | MR_ELEC | MR_SLEEP | MR_POISON
