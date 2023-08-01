39c2945da791d40d5c4d87156259b2f7abf8180e
Erik Lunna <eslunna@gmail.com>
2023-08-01 16:17:58 +0200

    Add reflection to Nighthorn when wielded.

--- src/artifact.c
+++ src/artifact.c
@@ -1063,6 +1063,7 @@ long wp_mask;
         } else if (otmp
             && (otmp->oartifact == ART_LONGBOW_OF_DIANA
                 || otmp->oartifact == ART_CROSSBOW_OF_CARL
+                || otmp->oartifact == ART_NIGHTHORN
                 || otmp->oartifact == ART_LIGHTSABER_PROTOTYPE)
             && (wp_mask & W_WEP)) { /* wielding various reflecting artifacts */
             if (on)
