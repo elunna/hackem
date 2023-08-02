2bc6523d1d89747d9d52c57e5962e142de0dc9ee
Erik Lunna <eslunna@gmail.com>
2023-08-02 10:50:19 +0200

    When boomerang is upgraded to chakram, don't change material unless necessary.

--- src/potion.c
+++ src/potion.c
@@ -3997,10 +3997,6 @@ register struct obj *obj;
         /*if (obj->spe > chg) obj->spe = chg;*/
         obj->known = 0;
         break;
-    case CHAKRAM:
-        /* Maybe change the material? */
-        warp_material(obj, TRUE);
-        break;
     case SADDLE:
         obj->corpsenm = -1;
         break;
