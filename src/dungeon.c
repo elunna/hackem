82fb36451cd99e7872e60441ed8f7e21d77c0722
Erik Lunna <eslunna@gmail.com>
2023-08-09 15:13:20 +0200

    Fix: Elemental Planes Overview (From EvilHack) Written by terrapin. Make ^O dungeon overview show the Planes in the correct, randomized order.

--- src/dungeon.c
+++ src/dungeon.c
@@ -3013,7 +3013,8 @@ boolean printdun;
     /* calculate level number */
     i = depthstart + mptr->lev.dlevel - 1;
     if (In_endgame(&mptr->lev))
-        Sprintf(buf, "%s%s:", TAB, endgamelevelname(tmpbuf, i));
+        Sprintf(buf, "%s%s:", TAB, 
+                endgamelevelname(tmpbuf, observable_depth(&mptr->lev)));
     else
         Sprintf(buf, "%sLevel %d:", TAB, i);
 
