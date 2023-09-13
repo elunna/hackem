53c01ea03dd83bd9b2c248a68d0f6b8bd40c3d8f
Erik Lunna <eslunna@gmail.com>
2023-09-13 18:26:58 +0200

    Fix: typo when a monster is protected from a clobber attack.

--- src/mhitm.c
+++ src/mhitm.c
@@ -1646,7 +1646,7 @@ struct obj **ootmp; /* to return worn armor for caller to disintegrate */
         if ((mattk->adtyp == AD_CLOB) && tmp > 0 
               && !unsolid(mdef->data)&& !rn2(6)) {
             if (defended(mdef, AD_CLOB)) {
-                pline("%s clobbers %s, but it's armor holds firm!",
+                pline("%s clobbers %s, but its armor holds firm!",
                     Monnam(magr), mon_nam(mdef));
             }
             else if (tmp < mdef->mhp) {
