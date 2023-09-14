bfa59105914d090a63a01fdc7ee55b49157286c3
Erik Lunna <eslunna@gmail.com>
2023-09-14 14:05:11 +0200

    Fix: warning: the comparison will always evaluate as ‘true’ (botl.c) (From EvilHack).

--- src/botl.c
+++ src/botl.c
@@ -2266,7 +2266,7 @@ boolean from_configfile;
         return parse_condition(s, sidx);
 
     ++sidx;
-    while (s[sidx]) {
+    while (s[sidx][0]) {
         char buf[BUFSZ], **subfields;
         int sf = 0;     /* subfield count */
         int kidx;
@@ -2691,7 +2691,7 @@ int sidx;
      */
 
     sidx++;
-    while(s[sidx]) {
+    while(s[sidx][0]) {
         int sf = 0;     /* subfield count */
         char buf[BUFSZ], **subfields;
 
