32587e3cf7926c39902a4a843573f8feee44ad5f
Erik Lunna <eslunna@gmail.com>
2023-09-25 22:19:22 +0200

    Ice mage skill updates.

--- src/u_init.c
+++ src/u_init.c
@@ -587,25 +587,20 @@ static const struct def_skill Skill_H[] = {
     { P_NONE, 0 }
 };
 static const struct def_skill Skill_I[] = {
-/*Resorts mostly to stabbing weapons*/
-    { P_DAGGER, P_SKILLED },
-    { P_KNIFE,  P_EXPERT },
+    /*Resorts mostly to stabbing weapons*/
+    { P_DAGGER, P_EXPERT },
+    { P_KNIFE,  P_SKILLED },
     { P_AXE, P_BASIC },
-    { P_PICK_AXE, P_BASIC },
-    { P_SHORT_SWORD, P_EXPERT },
-    { P_BROAD_SWORD, P_BASIC },
+    { P_PICK_AXE, P_SKILLED },
+    { P_SHORT_SWORD, P_SKILLED },
     { P_LONG_SWORD, P_BASIC },
-    { P_FLAIL, P_SKILLED },
-    { P_QUARTERSTAFF, P_BASIC },
-    { P_SPEAR, P_SKILLED },
-    { P_TRIDENT, P_EXPERT },
-/*  { P_BOW, P_BASIC },*/
-    { P_SLING, P_BASIC },
-/*  { P_CROSSBOW, P_BASIC },
-    { P_DART, P_EXPERT },*/
+    { P_QUARTERSTAFF, P_SKILLED },
+    { P_SPEAR, P_BASIC },
+    { P_TRIDENT, P_BASIC },
     { P_UNICORN_HORN, P_SKILLED },
 
     { P_ATTACK_SPELL, P_BASIC },
+    { P_DIVINATION_SPELL, P_BASIC },
     { P_ENCHANTMENT_SPELL, P_EXPERT },
     { P_ESCAPE_SPELL, P_BASIC },
     { P_MATTER_SPELL, P_EXPERT },
