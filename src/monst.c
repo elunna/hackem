ea9eb6a035a57594603068d0d56cb8c29d377065
Erik Lunna <eslunna@gmail.com>
2023-10-01 17:04:48 +0200

    All dragons can engulf attack again. With the introduction of the stench property, the player will have more possibilities to protect themselves.

--- src/monst.c
+++ src/monst.c
@@ -3446,9 +3446,8 @@ NEARDATA struct permonst mons[] = {
           ATTK(AT_BITE, AD_PHYS, 4, 8),
           ATTK(AT_CLAW, AD_PHYS, 4, 4),
           ATTK(AT_CLAW, AD_PHYS, 4, 4),
-          ATTK(AT_NONE, AD_STUN, 2, 6),
-          /*ATTK(AT_ENGL, AD_DGST, 2, 10)*/
-          NO_ATTK),
+          ATTK(AT_ENGL, AD_DGST, 2, 10),
+          ATTK(AT_NONE, AD_STUN, 2, 6)),
         SIZ(WT_DRAGON, NUT_DRAGON, MS_ROAR, MZ_GIGANTIC), MR_SONIC, 0,
         M1_FLY | M1_THICK_HIDE | M1_NOHANDS | M1_SEE_INVIS | M1_OVIPAROUS
           | M1_CARNIVORE,
@@ -3460,9 +3459,8 @@ NEARDATA struct permonst mons[] = {
           ATTK(AT_BITE, AD_DRLI, 4, 8),
           ATTK(AT_CLAW, AD_PHYS, 4, 4),
           ATTK(AT_CLAW, AD_PHYS, 4, 4),
-          ATTK(AT_NONE, AD_DRLI, 0, 1),
-          NO_ATTK),
-        /*ATTK(AT_ENGL, AD_DGST, 2, 10), */
+          ATTK(AT_ENGL, AD_DGST, 2, 10),
+          ATTK(AT_NONE, AD_DRLI, 0, 1)),
         SIZ(WT_DRAGON, NUT_DRAGON, MS_ROAR, MZ_GIGANTIC), MR_POISON | MR_SONIC, 0,
         M1_FLY | M1_THICK_HIDE | M1_NOHANDS | M1_SEE_INVIS | M1_OVIPAROUS
             | M1_CARNIVORE | M1_POIS,
@@ -3500,9 +3498,8 @@ NEARDATA struct permonst mons[] = {
           ATTK(AT_BITE, AD_PHYS, 4, 8),
           ATTK(AT_CLAW, AD_PHYS, 4, 4), 
           ATTK(AT_CLAW, AD_PHYS, 4, 4),
-          /*ATTK(AT_ENGL, AD_DGST, 2, 10), */
-          ATTK(AT_NONE, AD_SLOW, 0, 2),
-          NO_ATTK),
+          ATTK(AT_ENGL, AD_DGST, 2, 10),
+          ATTK(AT_NONE, AD_SLOW, 0, 2)),
         SIZ(WT_DRAGON, NUT_DRAGON, MS_ROAR, MZ_GIGANTIC), MR_SLEEP | MR_SONIC, MR_SLEEP,
         M1_FLY | M1_THICK_HIDE | M1_NOHANDS | M1_SEE_INVIS | M1_OVIPAROUS | M1_CARNIVORE,
         M2_HOSTILE | M2_STRONG | M2_NASTY | M2_GREEDY | M2_JEWELS | M2_MAGIC,
@@ -3513,9 +3510,8 @@ NEARDATA struct permonst mons[] = {
           ATTK(AT_BITE, AD_PHYS, 4, 8),
           ATTK(AT_CLAW, AD_PHYS, 4, 4),
           ATTK(AT_CLAW, AD_PHYS, 4, 4),
-          /*ATTK(AT_ENGL, AD_DGST, 2, 10), */
-          ATTK(AT_NONE, AD_LOUD, 0, 2),
-          NO_ATTK),
+          ATTK(AT_ENGL, AD_DGST, 2, 10),
+          ATTK(AT_NONE, AD_LOUD, 0, 2)),
         SIZ(WT_DRAGON, 1500, MS_ROAR, MZ_GIGANTIC), MR_SONIC, 0,
         M1_FLY | M1_THICK_HIDE | M1_NOHANDS | M1_SEE_INVIS | M1_OVIPAROUS | M1_CARNIVORE,
         M2_HOSTILE | M2_STRONG | M2_NASTY | M2_GREEDY | M2_JEWELS | M2_MAGIC,
@@ -3526,9 +3522,8 @@ NEARDATA struct permonst mons[] = {
           ATTK(AT_BITE, AD_PHYS, 4, 8),
           ATTK(AT_CLAW, AD_PHYS, 4, 4), 
           ATTK(AT_CLAW, AD_PHYS, 4, 4),
-          /*ATTK(AT_ENGL, AD_DGST, 2, 10), */
-          ATTK(AT_NONE, AD_DISN, 0, 0),
-          NO_ATTK),
+          ATTK(AT_ENGL, AD_DGST, 2, 10),
+          ATTK(AT_NONE, AD_DISN, 0, 0)),
         SIZ(WT_DRAGON, NUT_DRAGON, MS_ROAR, MZ_GIGANTIC), MR_DISINT | MR_SONIC, MR_DISINT,
         M1_FLY | M1_THICK_HIDE | M1_NOHANDS | M1_SEE_INVIS | M1_OVIPAROUS
             | M1_CARNIVORE,
@@ -3565,8 +3560,7 @@ NEARDATA struct permonst mons[] = {
           ATTK(AT_BITE, AD_PHYS, 4, 8),
           ATTK(AT_CLAW, AD_PHYS, 4, 4), 
           ATTK(AT_CLAW, AD_PHYS, 4, 4),
-          /*ATTK(AT_ENGL, AD_DGST, 2, 10), */
-          NO_ATTK,
+          ATTK(AT_ENGL, AD_DGST, 2, 10),
           NO_ATTK),
         SIZ(WT_DRAGON, NUT_DRAGON, MS_ROAR, MZ_GIGANTIC), MR_FIRE | MR_SONIC, 0,
         M1_FLY | M1_THICK_HIDE | M1_NOHANDS | M1_SEE_INVIS | M1_OVIPAROUS
