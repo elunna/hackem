829b09227021d4522de32b52c2b4eec17d3c8fe3
Erik Lunna <eslunna@gmail.com>
2023-10-03 16:38:05 +0200

    Changed the material of the Crown of Saint Edward to steel so yeoman elves can wear it.

--- include/artilist.h
+++ include/artilist.h
@@ -764,7 +764,7 @@ STATIC_OVL NEARDATA struct artifact artilist[] = {
     A("The Crown of Saint Edward", HELM_OF_TELEPATHY,
       (SPFX_NOGEN | SPFX_RESTR | SPFX_INTEL | SPFX_DEFN), (SPFX_HSPDAM), 0,
       NO_ATTK, DFNS(AD_MAGM), NO_CARY, 0,  
-      A_LAWFUL, PM_YEOMAN, NON_PM, 5000L, NO_COLOR, DEFAULT_MAT),
+      A_LAWFUL, PM_YEOMAN, NON_PM, 5000L, NO_COLOR, METAL),
     
     /*
      *      Based loosely off of the Ring of P'hul - from 'The Lords of Dus' series
