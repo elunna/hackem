ad4ddc4064745fd319781c95d5b2221b71d41b93
Erik Lunna <eslunna@gmail.com>
2023-10-09 18:06:59 +0200

    Player vampires will only polymorph into vampire bats or fog clouds if they hit a random polymorph.

--- src/polyself.c
+++ src/polyself.c
@@ -651,7 +651,8 @@ int psflags;
         } else if (isvamp) {
         do_vampyr:
             if (mntmp < LOW_PM || (mons[mntmp].geno & G_UNIQ))
-                mntmp = (youmonst.data != &mons[PM_VAMPIRE] && !rn2(10))
+                mntmp = (!maybe_polyd(is_vampire(youmonst.data), Race_if(PM_VAMPIRIC))
+                        && !rn2(10))
                             ? PM_WOLF
                             : !rn2(4) ? PM_FOG_CLOUD : PM_VAMPIRE_BAT;
             if (controllable_poly) {
