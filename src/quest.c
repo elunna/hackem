cebc8c49c51fd33c7a78c6d5845c065e915ccbb3
Erik Lunna <eslunna@gmail.com>
2023-10-12 14:38:03 +0200

    Relax the quest abuse threshold to -13. Above that and you get a boon, below it and you risk angering your quest leader.

--- src/quest.c
+++ src/quest.c
@@ -265,7 +265,7 @@ struct obj *obj; /* quest artifact; possibly null if carrying Amulet */
         /* leader IDs the real amulet but ignores any fakes */
         if ((otmp = carrying(AMULET_OF_YENDOR)) != 0)
             fully_identify_obj(otmp);
-    } else if (u.ualign.abuse != 0) { /* player has abused their alignment */
+    } else if (u.ualign.abuse <= -13) { /* player has abused their alignment */
         /* the more often the player abuses their alignment,
            the greater the odds of their quest leader demanding
            that they forfeit the quest artifact */
@@ -370,7 +370,7 @@ struct obj *obj; /* quest artifact; possibly null if carrying Amulet */
         /* Since the player is punished for abusing alignment, let's reward them
          * for "walking the line", with a permanent +1 luck bonus (credit to
          * mobileuser for this idea). */
-        if (!alignabuse && u.ualign.abuse >= -13) {
+        if (!alignabuse && u.ualign.abuse > -13) {
             flags.quest_boon = TRUE;
             change_luck(10);
             pline("Before departing, you hear some strange whispering. You feel your luck changing!");
