f194a80f510ce308b12e22d6e54bdff1d3af24ea
Erik Lunna <eslunna@gmail.com>
2023-10-23 22:57:36 +0200

    Priests reduce the chance of zombie revival (DynaHack). This is an interesting role differentiation change - I like it because it's similar to the Undead Slayer's zombie squishing, but more preventative.
    In Dynahack, the odds of preventing a zombie from reviving was 50%, I bumped it up to 66%.

--- src/mkobj.c
+++ src/mkobj.c
@@ -1467,7 +1467,9 @@ struct obj *body;
             }
         }
     /* corpse of an actual zombie */
-    } else if (body->zombie_corpse && !body->norevive) {
+    } else if (body->zombie_corpse && !body->norevive
+          /* Priests have a chance to put down zombies for good. */
+          && !(Role_if(PM_PRIEST) && rn2(3))) {
         for (age = 2; age <= ROT_AGE; age++) {
             if (!rn2(ZOMBIE_REVIVE_CHANCE)) { /* zombie revives */
                 action = ZOMBIFY_MON; /* if buried, can dig itself out */
