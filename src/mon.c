17f0bccb3b91fb50f60f15745f6d889064cd2da8
Erik Lunna <eslunna@gmail.com>
2023-11-08 09:40:39 +0100

    Revert "Long worms only drop a worm tooth 5% of the time now when killed."
    This reverts commit a8f14407109f2eec674b94c8c6f3711f1f82eb28.

--- src/mon.c
+++ src/mon.c
@@ -649,10 +649,7 @@ unsigned corpseflags;
         }
         goto default_1;
     case PM_LONG_WORM:
-        /* Crysknives remain the same, just much smaller chance of getting 
-         * a worm tooth from a long worm. */
-        if (!rn2(20 - Luck))
-            (void) mksobj_at(WORM_TOOTH, x, y, TRUE, FALSE);
+        (void) mksobj_at(WORM_TOOTH, x, y, TRUE, FALSE);
         goto default_1;
     case PM_KILLER_TRIPE_RATION:
 		obj = mksobj_at(TRIPE_RATION, x, y, TRUE, FALSE);
