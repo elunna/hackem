8bb089943b2ff49a5a670e35a515d2413a8cbb4c
Erik Lunna <eslunna@gmail.com>
2023-11-17 13:12:01 +0100

    Fix: Move check for zapping scroll in heart of the cards tech to correct spot.

--- src/tech.c
+++ src/tech.c
@@ -4072,13 +4072,13 @@ tech_heartcards()
                 if (j == SCR_AMNESIA || j == SCR_PUNISHMENT
                     || j == SCR_DESTROY_ARMOR)
                     continue;
-                else if (j == SCR_ZAPPING) /* Too random? */
-                    continue;
                 else if (j == SCR_ENCHANT_WEAPON && !uwep)
                     continue;
                 else if (j == SCR_ENCHANT_WEAPON && uwep && uwep->spe > 5)
                     continue;
             }
+            if (j == SCR_ZAPPING) /* Too random? */
+                continue;
             if (j)
                 break;
         }
